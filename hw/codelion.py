2b224056acc3ef391b1682e1c21b427bbed03028
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-05-06 02:22:41 +0900

    menu_py

--- hw/codelion.py
+++ hw/codelion.py
@@ -1,43 +1,30 @@
-import random
 import time
+import random
 
-list = ["된장찌개","피자","제육볶음","치킨","떡볶이"]
-list = "냉장고"
-
-for i in range(30):
-    print(random.choice(list))
-
+lunch = ["된장찌개", "피자", "햄버거", "제육볶음", "짜장면"]
 while True:
-    print(random.choice(list))
+    menu = input("추가하실 메뉴를 입력하세요")
+    if menu == "q":
+        break
+    else:
+        lunch.append(menu)
+
+lunch_set = set(lunch)
+print(lunch)
+
+
+set_lunch = set(lunch)
+while True:
+    item = input("삭제하실 음식을 선택해주세요")
+    if item == "q":
+        break
+    else:
+        set_lunch = set_lunch - set([item])
+
+print(set_lunch, "중에서 선택합니다.")
+for i in range(5, 0, -1):
+    print(i)
     time.sleep(1)
-    break
-
-dict = {"고향":"수원", "취미":"영화보기", "음식":["복숭아", "사과", "감"]}
-info = {"특기":"피아노", "사는 곳":"서울"}
-
-for i in info.items():
-    print(i, type(i))
 
 
-#set
-foods = ["된장찌개", "사과", "된장찌개", "복숭아", "율루"]
-food2 = ["된장찌개", "복숭아", "삽겹살", "곱창"]
-food1 = set(foods)
-food2 = set(food2)
-print(foods)
-print(set(foods))
-#합집합
-print(food1 | food2)
-#교집합
-print(food1 & food2)
-#차집합
-print(food1 - food2)
-
-
-chose = random.choice(["된장찌개", "피자", "제육볶음"])
-if chose == "제육볶음":
-    print("곱빼기 주세요")
-else:
-    print("그냥 주세요")
-
-print("종료")
+print(random.choice(list(set_lunch)))
