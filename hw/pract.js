2cd46991df0378567654bbcd6ef3b25d0a6fb15e
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-05-19 19:12:32 +0900

    bar-tap

--- hw/pract.js
+++ hw/pract.js
@@ -1,35 +1,23 @@
-document.getElementById("lion").style.color = "blue";
-document.getElementById("tiger").style.color = "red";
-document.getElementsByTagName("li")[0].style.border = "1px yellow solid";
-document.getElementsByClassName("animal")[0].style.backgroundColor = "orange"
-document.querySelectorAll("li")[1].style.fontSize = "20px";
-
-document.querySelector("#tiger").innerHTML = "바꾸었지롱";
-const li = document.createElement("li");
-const box = document.querySelector(".container");
-box.appendChild(li);
-li.innerText = "heelod";
-box.innerHTML += "<li class='add'>또 추가했다</li>"
-
-//이벤트 타입
-const btn = document.querySelector(".btn");
-btn.addEventListener("click", function () {
-    document.querySelector("div").classList.toggle("color");
-    btn.innerText = "눌렸어";
+const list = document.querySelector(".list");
+list.addEventListener("click", function (e) {
+    tab(e.target.dataset.id);
+    console.log(e.target.dataset.id);
 })
 
+function tab(e) {
+    const tabBtn = document.querySelectorAll(".tab-btn");
+    const tabBox = document.querySelectorAll(".tab-box");
+    for (let i = 0; i < tabBtn.length; i++) {
+        tabBtn[i].classList.remove("active");
+        tabBox[i].classList.add("hidden");
+    }
+    tabBtn[e].classList.add("active");
+    tabBox[e].classList.remove("hidden");
+}
 
-const fontPlus = document.querySelector(".font-plus");
-const fontMius = document.querySelector(".font-mius");
-const font = document.querySelector(".font > span");
-let fontNum = 16;
-fontPlus.addEventListener("click", function () {
-    fontNum += 1;
-    font.style.fontSize = `${fontNum}px`;
-})
-
-fontMius.addEventListener("click", function () {
-    fontNum -= 1;
-    font.style.fontSize = `${fontNum}px`;
-
-})
+// for (let i = 0; i < tabBtn.length; i++) {
+//     tabBtn[i].addEventListener("click", function () {
+//         tabBtn[i].classList.toggle("active");
+//         tabBox[i].classList.toggle("hidden");
+//     })  
+// };
